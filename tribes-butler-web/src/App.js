26be9373df80afbb73be59d3a4b92cc83499af70
Suxiang Han <suxianghan@outlook.com>
2022-12-13 20:39:30 -0600

    Calibration Line

--- tribes-butler-web/src/App.js
+++ tribes-butler-web/src/App.js
@@ -9,11 +9,29 @@ export default function App() {
   const [selectedFile, setSelectedFile] = useState(null);
  
   const drawRectangle = () => {
+    if(selectedFile == null){return}
+
+
     const context = canvasRef.current.getContext("2d");
-    context.strokeStyle = "white";
-    context.lineWidth = 2;
-    context.strokeRect(50, 30, 110, 90);
-    context.strokeRect(170, 65, 100, 80);
+
+    
+    const canvasBG = new Image(600,400);
+    canvasBG.src= URL.createObjectURL(selectedFile);
+    canvasBG.onload = () => {
+      var imgwidth = canvasBG.width;
+      var imgheight = canvasBG.height;
+      console.log(imgheight, imgwidth)
+      context.drawImage(canvasBG, 0, 0, 600, 500);
+
+      context.strokeStyle = "red";
+      context.lineWidth = 2;
+      context.strokeRect(0, 0, 600, 500);
+      for (let i = 0; i < 500; i+=100) {
+        context.strokeRect(i, 0, 100, 500);
+        context.strokeRect(0, i, 600, 100);
+      }
+    } 
+
   };
   
   const fetchPlayerScoreResponse = () => {
@@ -33,26 +51,39 @@ export default function App() {
         console.error(error);
     });
   };
- 
+
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   canvasBgRef.onload = () => {
+  //     console.log("?");
+  //     const context = canvasRef.current.getContext("2d");
+  //     context.drawImage(canvasBgRef, 0, 0, 600, 400)
+  //     drawRectangle();
+  //   }
+  // });
+
+  useEffect(() => {
+    drawRectangle(); // This is be executed when `loading` state changes
+  });
+
   return (
     <div>
       <h1>Open up App.js to start working on your app!</h1>
       <div>
-        <input type="file" name="file" onChange={(e) => {setSelectedFile(e.target.files[0]); drawRectangle();}}/>
+        <canvas
+          ref={canvasRef}
+          width="600" 
+          height="500"
+        />
+      </div>
+      <div>
+        <input type="file" name="file" onChange={(e) => {
+          setSelectedFile(e.target.files[0]);
+        }}/>
         <button onClick={() => fetchPlayerScoreResponse()}> SB </button>
         <Button type="primary" onClick={() => fetchPlayerScoreResponse()}>Primary Button</Button>
       </div>
       <h1>{data==null ? null : data.score}</h1>
-      <div>
-        <canvas
-          ref={canvasRef}
-          style={{
-            width: "600px",
-            height: "400px",
-            background: selectedFile,
-          }}
-      />
-      </div>
     </div>
   );
 }
